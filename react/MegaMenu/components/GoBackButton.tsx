3d177c1494a60ba10c648f029b995b182beb4265
Paulo Ribeiro <pauloribeiro@Paulos-Mac-mini.local>
2022-03-03 12:40:20 -0500

    removed some comments

--- react/MegaMenu/components/GoBackButton.tsx
+++ react/MegaMenu/components/GoBackButton.tsx
@@ -6,17 +6,9 @@ import type { InjectedIntlProps } from 'react-intl'
 import { injectIntl } from 'react-intl'
 import { useCssHandles } from 'vtex.css-handles'
 
-// import { Icon } from 'vtex.store-icons'
 import backarrow from '../../../assets/backarrow.svg'
 
 import { megaMenuState } from '../State'
-
-// const messages = defineMessages({
-//   goBackButtonTitle: {
-//     defaultMessage: '',
-//     id: 'store/mega-menu.goBackButton.title',
-//   },
-// })
 
 const CSS_HANDLES = [
   'goBackContainer',
